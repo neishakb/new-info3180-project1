c1fc3d16edfd0a49a1671fc0441c26100bc7170f
Ubuntu <ubuntu@ip-172-31-42-240.ec2.internal>
2020-04-11 23:33:41 +0000

    changed driver for URI

--- app/__init__.py
+++ app/__init__.py
@@ -6,7 +6,7 @@ from flask_migrate import Migrate, MigrateCommand
 
 app = Flask(__name__)
 app.config['SECRET_KEY'] = "12345"
-app.config['SQLALCHEMY_DATABASE_URI'] = "postgresql://project1:project1@localhost/project1"
+app.config['SQLALCHEMY_DATABASE_URI'] = "postgresql://rvmyeeekigwzeo:60ccf2452b672b9bbe5ff2d076ad6ca8a64dac3432f73b5973de6b0f6f4f18ca@ec2-54-157-78-113.compute-1.amazonaws.com:5432/d4io25h9cd94m2"
 app.config['SQLALCHEMY_TRACK_MODIFICATIONS'] = False # added just to suppress a warning
 
 db = SQLAlchemy(app)
